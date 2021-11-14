16bf58acaf37f09a24d62a8716ebfb10092e6ca6
husseinfk <54551677+husseinfk@users.noreply.github.com>
2021-11-13 19:15:14 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -38,9 +38,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
 
 module.exports = app;
