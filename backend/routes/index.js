3512dbf20909a814879a5a9529ac6c8adc6101f3
husseinfk <54551677+husseinfk@users.noreply.github.com>
2021-11-13 19:06:48 -0500

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("HELLO WORLD...");
+  res.send("HELLO WORLDS...");
 });
 
 module.exports = router;
