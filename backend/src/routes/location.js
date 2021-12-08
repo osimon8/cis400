d329d784d0bf38733e04be0dd92c550e85247a28
Owen Simon <osimon922@gmail.com>
2021-12-07 23:25:11 -0500

    added route to get all locations, updated location schema

--- backend/src/routes/location.js
+++ backend/src/routes/location.js
@@ -5,20 +5,30 @@ var geolib = require("geolib");
 const { database } = require("../database/db");
 
 router.post("/set", async function (req, res, next) {
-  const { userId, coordinates } = req.body;
-  if (!userId || !coordinates) {
+  const { userId, latitude, longitude } = req.body;
+  if (!userId || !latitude || !longitude) {
     res.statusCode = 400;
     res.send("Missing user ID or coordinates");
     return;
   }
-  const valid_coords = geolib.isSexagesimal(coordinates);
+  const coords = {
+    latitude: parseFloat(latitude),
+    longitude: parseFloat(longitude),
+  };
+  const valid_coords = geolib.isValidCoordinate(coords);
   if (!valid_coords) {
     res.status(400).send("Invalid coordinate format");
     return;
   }
   database.query(
-    "INSERT INTO LOCATION (user_id,coordinates) VALUES (?,?) ON DUPLICATE KEY UPDATE coordinates=?;",
-    [userId, coordinates, coordinates],
+    "INSERT INTO LOCATION (user_id,latitude,longitude) VALUES (?,?,?) ON DUPLICATE KEY UPDATE latitude=?,longitude=?;",
+    [
+      userId,
+      coords.latitude,
+      coords.longitude,
+      coords.latitude,
+      coords.longitude,
+    ],
     function (error) {
       if (error) {
         console.log(error);
@@ -28,6 +38,17 @@ router.post("/set", async function (req, res, next) {
       }
     }
   );
+});
+
+router.get("/get", async function (req, res, next) {
+  database.query("SELECT * FROM LOCATION", function (error, results) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(500);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 module.exports = router;
