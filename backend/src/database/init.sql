42d984f54ed9b7f4135dbc3c74a9d6f4d50ecad6
Owen Simon <osimon922@gmail.com>
2022-04-02 13:31:12 -0400

    added location sharing

--- backend/src/database/init.sql
+++ backend/src/database/init.sql
@@ -3,7 +3,7 @@ CREATE TABLE `User`.`CHATS` (
   `userId` VARCHAR(128) NOT NULL,
   `friendId` VARCHAR(128) NOT NULL,
   `text` VARCHAR(512) NULL,
-  `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`userId`, `friendId`, `timestamp`),
   CONSTRAINT `friends`
     FOREIGN KEY (`userId`, `friendId`)
@@ -19,6 +19,25 @@ CREATE TABLE `User`.`ONLINE` (
   UNIQUE INDEX `idOnline_UNIQUE` (`id` ASC) VISIBLE,
   CONSTRAINT `fk id`
     FOREIGN KEY (`id`)
+    REFERENCES `User`.`USERS` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
+
+---SHARING
+CREATE TABLE `User`.`SHARING` (
+  `userId` VARCHAR(128) NOT NULL,
+  `friendId` VARCHAR(128) NOT NULL,
+  `timestamp` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`, `friendId`),
+  INDEX `uid-sharing` (`userId` ASC) INVISIBLE,
+  INDEX `fid-sharing` (`friendId` ASC) VISIBLE,
+  CONSTRAINT `uid-sharing`
+    FOREIGN KEY (`userId`)
+    REFERENCES `User`.`USERS` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fid-sharing`
+    FOREIGN KEY (`friendId`)
     REFERENCES `User`.`USERS` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION);
